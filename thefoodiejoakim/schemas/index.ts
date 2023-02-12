44ff5050fcff0c086076c13124b89b6c86bce9b5
Joakim Vu <joakimvu@icloud.com>
2023-02-12 01:36:36 +0100

    Update index.ts
    Created all schema needed for Recipe

--- thefoodiejoakim/schemas/index.ts
+++ thefoodiejoakim/schemas/index.ts
@@ -2,5 +2,24 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import country from './country'
+import recipe from './recipe'
+import cookingTime from './cookingTime'
+import protein from './protein'
+import cookingMethod from './cookingMethod'
+import mealType from './mealType'
+import season from './season'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  country,
+  recipe,
+  cookingTime,
+  protein,
+  cookingMethod,
+  mealType,
+  season,
+]
