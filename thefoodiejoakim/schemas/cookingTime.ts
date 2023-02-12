c8b2d782e81936c2ee6b353fde6c341881a4f4cd
Joakim Vu <joakimvu@icloud.com>
2023-02-12 01:36:10 +0100

    Create cookingTime.ts

---
+++ thefoodiejoakim/schemas/cookingTime.ts
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'cookingTime',
+  title: 'Coooking Time',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'cookingTime',
+      title: 'Cooking Time',
+      type: 'string',
+    }),
+  ],
+})
