21fd8fa053ee8e8c59b625a2f5dbfdc7a91765dd
Joakim Vu <joakimvu@icloud.com>
2023-02-12 18:34:32 +0100

    Update index.tsx

--- frontend/pages/oppskrifter/index.tsx
+++ frontend/pages/oppskrifter/index.tsx
@@ -5,18 +5,33 @@ import groq from "groq";
 import client from "../../client";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
+import Image from "react-bootstrap/Image";
+
 const Index = ({ recipe }) => {
+  recipe = recipe[0];
   // How to get the cookingSteps style
   // Where you iterate over cookingSteps[number]
-  console.log(recipe[0].cookingSteps[1].style);
+  //   console.log(recipe[0].cookingSteps[1].style);
   // How to get the cookingSteps text
   // Where you iterate over cookingSteps[number].text
-  console.log(recipe[0].cookingSteps[1].children[0].text);
+  //   console.log(recipe[0].cookingSteps[1].children[0].text);
 
+  console.log(recipe);
   return (
-    <div>
-      <h1>Welcome to a recipe!</h1>
-    </div>
+    <main className="container mt-5" style={{ maxWidth: "768px" }}>
+      <Image src={recipe?.imageUrl} style={{ width: "100%" }} />
+      <div className="row">
+        <div className="col-sm-6">
+          <h4>{recipe?.country}</h4>
+        </div>
+        <div className="col-sm-6">
+          <h4 className="text-end">{recipe?.cookingTime}</h4>
+        </div>
+      </div>
+      <p>Sist oppdatert: {recipe?.publishedAt}</p>
+      <h1>{recipe?.title}</h1>
+      <p>{recipe?.description}</p>
+    </main>
   );
 };
 
