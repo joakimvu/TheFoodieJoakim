6cde153f4f92fa339a7d92a50d63e52563d64229
joakimvu <joakim.vu@hiof.no>
2023-02-12 23:16:03 +0100

    Create unit.ts

---
+++ thefoodiejoakim/schemas/unit.ts
@@ -0,0 +1,20 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'unit',
+  title: 'Unit',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+})
