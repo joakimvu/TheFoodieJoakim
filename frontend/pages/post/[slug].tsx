6799931c4fc9862e0d8ecfe97bd0248c65ba55b6
joakimvu <joakim.vu@hiof.no>
2023-02-13 17:07:53 +0100

    Update [slug].tsx

--- frontend/pages/post/[slug].tsx
+++ frontend/pages/post/[slug].tsx
@@ -3,7 +3,7 @@
 import groq from "groq";
 import imageUrlBuilder from "@sanity/image-url";
 import { PortableText } from "@portabletext/react";
-import client from "../../client";
+import client from "../../services/client";
 
 function urlFor(source) {
   return imageUrlBuilder(client).image(source);
