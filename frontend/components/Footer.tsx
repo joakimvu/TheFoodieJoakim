eb4b0aa0bc674feb3c844c717c4864074f719862
Joakim Vu <joakimvu@icloud.com>
2023-02-18 17:58:49 +0100

    Create Footer.tsx

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  // TODO:
+  // Legg til "Send feedback" som linker til mail?
+  // Må legge til tilgjengelighetserklæring
+  // Privacy Policy?
+  return <div>Copyright 2023 - @Joakim Vu</div>;
+}
