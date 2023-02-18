3fd67a834db45560f22b8921514dfd076c1d927d
Joakim Vu <joakimvu@icloud.com>
2023-02-18 17:58:56 +0100

    Update index.tsx

--- frontend/pages/oppskrifter/index.tsx
+++ frontend/pages/oppskrifter/index.tsx
@@ -6,9 +6,10 @@ import client from "../../services/client";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import Image from "react-bootstrap/Image";
+import FilterRecipes from "@/components/FilterRecipes";
+import { Card } from "react-bootstrap";
 
-const Index = ({ recipe }: any) => {
-  recipe = recipe[0];
+const Index = ({ recipes }: any) => {
   // How to get the cookingSteps style
   // Where you iterate over cookingSteps[number]
   //   console.log(recipe[0].cookingSteps[1].style);
@@ -16,57 +17,74 @@ const Index = ({ recipe }: any) => {
   // Where you iterate over cookingSteps[number].text
   //   console.log(recipe[0].cookingSteps[1].children[0].text);
 
-  console.log(recipe.ingredients);
-  // console.log(recipe?.ingredients[0].ingredientName);
+  console.log(recipes);
   return (
     <main className="container mt-5" style={{ maxWidth: "768px" }}>
-      <Image
-        src={recipe?.imageUrl}
-        alt={recipe?.title}
-        style={{ width: "100%" }}
-      />
-      <div className="row">
-        <div className="col-sm-6">
-          <h4>{recipe?.country}</h4>
-        </div>
-        <div className="col-sm-6">
-          <h4 className="text-end">{recipe?.cookingTime}</h4>
-        </div>
-      </div>
-      <div className="row">
-        <p>Sist oppdatert: {recipe?.publishedAt}</p>
-        <h1>{recipe?.title}</h1>
-        <p>{recipe?.description}</p>
-      </div>
+      <FilterRecipes />
+      {recipes?.map((recipe, index): any => (
+        <Card key={index} className="shadow p-3 mb-5 bg-white rounded">
+          <Link
+            href={`/oppskrifter/${recipe.slug.current}`}
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <Image
+              src={recipe?.imageUrl}
+              alt={recipe?.title}
+              style={{ width: "100%", maxHeight: "400px", objectFit: "cover" }}
+            />
+            <div className="row">
+              <div className="col-sm-6">
+                <h6>{recipe?.country}</h6>
+              </div>
+              <div className="col-sm-6">
+                <h6 className="text-end">{recipe?.cookingTime}</h6>
+              </div>
+            </div>
+            <div className="row">
+              <p>
+                Sist oppdatert:{" "}
+                {recipe?.publishedAt
+                  ? new Date(recipe.publishedAt).toLocaleDateString("no-NO", {
+                      day: "2-digit",
+                      month: "long",
+                      year: "numeric",
+                    })
+                  : ""}
+              </p>
+              <h1>{recipe?.title}</h1>
+            </div>
+          </Link>
+        </Card>
+      ))}
     </main>
   );
 };
 
 export async function getStaticProps() {
-  const recipe = await client.fetch(groq`
-  *[_type == "recipe"]{
-    title,
-    slug,
-    "imageUrl": mainImage.asset->url,
-    publishedAt,
-    "description": description[0].children[0].text,
-    "country": country.Country->title,
-    "cookingTime": cookingTime.Time->cookingTime,
-    "protein": protein[]->title,
-    "cookingMethod": cookingMethod[]->cookingMethod,
-    "mealType": mealType[]->title,
-    "season": season[]->title,
-    "cookingSteps": cookingSteps[],
-    "ingredients":   ingredients[] {
-      "ingredientName": ingredientName->title,
-      quantity,
-      "unitName": unit->title,
-    }
-  }
-      `);
+  const recipes = await client.fetch(groq`
+    *[_type == "recipe"]{
+      title,
+      slug,
+      "imageUrl": mainImage.asset->url,
+      publishedAt,
+      "description": description[0].children[0].text,
+      "country": country.Country->title,
+      "cookingTime": cookingTime.Time->cookingTime,
+      "protein": protein[]->title,
+      "cookingMethod": cookingMethod[]->cookingMethod,
+      "mealType": mealType[]->title,
+      "season": season[]->title,
+      "cookingSteps": cookingSteps[],
+      "ingredients":   ingredients[] {
+        "ingredientName": ingredientName->title,
+        quantity,
+        "unitName": unit->title,
+      }
+    }`);
+
   return {
     props: {
-      recipe,
+      recipes,
     },
   };
 }
