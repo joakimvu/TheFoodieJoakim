3a86f04694d9aac501bb7ed93c331ef820ea15dc
Joakim Vu <joakimvu@icloud.com>
2023-02-19 10:40:34 +0100

    Update getRecipe.tsx

--- frontend/services/getRecipe.tsx
+++ frontend/services/getRecipe.tsx
@@ -4,7 +4,7 @@ import groq from "groq";
 export const getRecipe = async (id: String) => {
   const pid = "mandu";
   const data = await client.fetch(groq`
-    *[_type == "recipe" && slug.current == "${id}"]{
+    *[_type == "recipe" && slug.current == "${id}"] | order(publishedAt desc){
       title,
       slug,
       "imageUrl": mainImage.asset->url,
