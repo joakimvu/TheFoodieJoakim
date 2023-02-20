6cb9f2754c159cecd013b446b16269e62e0fb628
Joakim Vu <joakimvu@icloud.com>
2023-02-20 12:51:00 +0100

    Update Navigation.tsx

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -4,11 +4,11 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 export default function Navigation() {
   return (
     <Navbar
-      className="px-4 py-2"
+      className="px-4 py-2 "
       collapseOnSelect
       expand="lg"
-      bg="dark"
-      variant="dark"
+      bg="light"
+      variant="light"
     >
       <Navbar.Brand href="/">
         {/* <Logo
@@ -17,11 +17,11 @@ export default function Navigation() {
           height="30"
           className="d-inline-block align-top"
         /> */}
-        <span className="badge bg-success">TFJ</span> The Foodie Joakim
+        <span className="badge bg-success ">TFJ</span> The Foodie Joakim
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ms-auto">
+        <Nav className="ms-auto ">
           <Nav.Link href="/">Hjem</Nav.Link>
           <Nav.Link href="/oppskrifter">Oppskrifter</Nav.Link>
           {/* <Nav.Link href="/ingredienser">Ingredienser</Nav.Link> */}
